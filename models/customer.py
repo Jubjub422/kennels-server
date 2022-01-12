7e1bf57d278bda04800d5d294c972b2b7f9edcdd
Jubjub422 <ross.stovenour@gmail.com>
2022-01-12 15:16:28 -0600

    added models

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer():
+    
+    
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
---
+++ models/employee.py
@@ -0,0 +1,6 @@
+class Employee():
+    
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
